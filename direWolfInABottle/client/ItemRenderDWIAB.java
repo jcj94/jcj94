56e16feec400ea2e3e3a5850e231b2820b845a15
Wolfie-Jcj94 <jcj94@ymail.com>
2013-03-29 15:59:41 -0400

    Commit 25 - 4:00
    May be the last commit before a final release

--- direWolfInABottle/client/ItemRenderDWIAB.java
+++ direWolfInABottle/client/ItemRenderDWIAB.java
@@ -1,10 +1,6 @@
 package jcj94.direWolfInABottle.client;
 
-import jcj94.direWolfInABottle.model.ModelDireWolfInABottle;
-
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.util.glu.Sphere;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.*;
 import net.minecraft.client.renderer.entity.RenderManager;
@@ -12,10 +8,13 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.IItemRenderer;
+import jcj94.direWolfInABottle.model.ModelDireWolfInABottle;
 
 public class ItemRenderDWIAB implements IItemRenderer 
 {
 
+
+	protected ModelDireWolfInABottle bottleModel;
 
 	public ItemRenderDWIAB()
 	{
@@ -47,6 +46,7 @@ public class ItemRenderDWIAB implements IItemRenderer
 			Minecraft.getMinecraft().renderEngine.bindTexture("/jcj94/direWolfInABottle/model/DWIAB.png");
 			if(!((EntityPlayer)data[1] == Minecraft.getMinecraft().renderViewEntity && Minecraft.getMinecraft().gameSettings.thirdPersonView == 0 && !((Minecraft.getMinecraft().currentScreen instanceof GuiInventory || Minecraft.getMinecraft().currentScreen instanceof GuiContainerCreative) && RenderManager.instance.playerViewY == 180.0F)))
 			{
+				
 				GL11.glPushMatrix();
 
 				GL11.glRotatef(0, 1.00F, 0.00F, 0.00F);
@@ -55,11 +55,13 @@ public class ItemRenderDWIAB implements IItemRenderer
 
 				GL11.glRotatef(0, 0.00F, 0.00F, 1.00F);
 
-				float scale = 1.00F;
+				float scale = 0.4F;
 
 				GL11.glScalef(scale, scale, scale);	
-
-				GL11.glTranslatef(-0.025F, 0.05F, 0.70F);
+				
+				GL11.glTranslatef(0F, 0F, 0F);
+				
+				bottleModel.render((Entity)data[1], 0.0f, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
 
 				GL11.glPopMatrix();
 			}
